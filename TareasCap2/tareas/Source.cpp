7f60733b0481bbbf964fc71b9281fa471dc73b74
AlexToscano <39318171+AlexToscano@users.noreply.github.com>
2018-09-06 23:50:53 -0500

    ejercicios del capitulo 2

---
+++ TareasCap2/tareas/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Includes.h"
+
+int main()
+{
+	cout << "Hello Progamming!" << endl;
+	cout << "Here we go!" << endl;
+	cin.get();
+
+	cout << "Instrucciones de ir a un baño" << endl;
+	cout << "1.-levantarse de de donde estes" << endl;
+	cout << "2.-caminar al fondo de la casa" << endl;
+	cout << "3.-girar hacia la derecha" << endl;
+	cout << "4.- Listo llegaste" << endl;
+	cin.get();
+
+	cout << "Instrucciones para ir al escuela" << endl;
+	cout << "1.-salir por la puerta de la casa" << endl;
+	cout << "2.-agarrar calle abajo por 4 cuadras hasta llegar a un parque" << endl;
+	cout << "3.-voltear hacia la dereche y caminar 2 cuadras" << endl;
+	cout << "4.-llegaste a la escuela" << endl;
+	cin.get();
+
+	cout << "Instrucciones para hacerte un plato de cereal" << endl;
+	cout << "1.-tomar un plato hondo";
+	cin.get();
+	cout << "2.-tomar la leche de tu preferencia y vertir en el plato hondo";
+	cin.get();
+	cout << "3.-por ultimo servir tu cereal favortio en el plato";
+	cin.get();
+	cout << "Provecho :)" << endl;
+	cin.get();
+
+
+}
